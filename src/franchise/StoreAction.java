1532e7822ed6fcb44bc7a60ba69f5d2fab505d5a
tjdanqkr <tjdanqkr@gmail.com>
2024-09-24 16:19:57 +0900

    abstract

--- src/franchise/StoreAction.java
+++ src/franchise/StoreAction.java
@@ -11,7 +11,9 @@ public interface StoreAction {
         메뉴의 총 가격은 3000원 입니다.
      */
     void print();
+    // 메뉴 추가 하면 totalprice 계산 알아서, 평균도 알아서 , 메뉴 수 도 알아서
     boolean addMenu(Food food);
+    // 메뉴 삭제 하면 totalprice 계산 알아서, 평균도 알아서 , 메뉴 수 도 알아서
     boolean removeMenu(String name);
 //   메뉴들 {아메리카노, 1000, {샷,얼음,물} }, {라떼, 2000, {샷,얼음,우유} }
     String getMenuArr();
