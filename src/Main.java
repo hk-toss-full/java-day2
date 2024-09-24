e30a3c510c61d17ad7dadada7ba57ff2732b1aa8
tjdanqkr <tjdanqkr@gmail.com>
2024-09-24 13:35:35 +0900

    constructor

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Team team = new Team();
-        team.name = "아메";
-        team.goal = "차카게";
-
-        Person2 p1 = new Person2();
-        Person2 p2 = new Person2();
-        Person2 p3 = new Person2();
-        p1.name = "김";
-        p1.age = 8;
-        p2.name = "이";
-        p2.age = 2;
-        p3.name = "박";
-        p3.age = 44;
-
-        team.members = new Person2[]{p1, p2, p3, null};
-        //Person p =new Person()
-// p.name = "최"
-// p.age = 21
-// team.memberAdd(p)
-// team.remove("이세연")
-// 이세연 remove
-// team.print()
-// 이 팀명은 아메리카노 입니다
-// 목표는 착하게 살자입니다
-// 팀원은 {"백승일", 22}, {"조예은",3}, {"최승혁", 21}
+        Person2 p1 = new Person2("11" , 123);
+        System.out.println(p1.name);
+        System.out.println(p1.age);
     }
 }
--- src/Person2.java
+++ src/Person2.java
@@ -2,11 +2,26 @@ public class Person2 {
     String name = "우재남";
     int age = 30;
     public String toCustomString() {
-        int agePlus1 = age + 1;
-        return "name : " + name + ", age : " + agePlus1;
+        return "name : " + name + ", age : ";
     }
     public void print(){
         System.out.println(toCustomString());
     }
+    // 생성자(constructor) 를 우리가 만들자
+//    AllArgumentConstructor
+    public Person2(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    // 이름은 같은데 아규먼트를 다르게 설계해서 재사용성을 늘리는 것을 overloading
+//    public Person2(String name){
+//        this.name = name;
+//        this.age = 0;
+//    }
+//    public Person2(int age){
+//        this.name = "";
+//        this.age = age;
+//    }
+//    public Person2(){}
 
 }
